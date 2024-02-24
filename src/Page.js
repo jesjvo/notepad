de725426166955040360b3e78ebd1820d587bcc0
jesjvo <jesvo5821@gmail.com>
2024-02-24 15:16:24 +1000

    update

--- src/Page.js
+++ src/Page.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import { FloatingMenu, useEditor, EditorContent, BubbleMenu } from '@tiptap/react'
+import React, {useState, useEffect, useRef} from 'react'
+import { useEditor, EditorContent, BubbleMenu } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Underline from '@tiptap/extension-underline';
 import TextAlign from '@tiptap/extension-text-align';
@@ -11,20 +11,98 @@ import './Page.css'
 import './App.css';
 
 //icons
-import { FaCaretLeft, FaFolderOpen, FaSave } from "react-icons/fa";
+import { FaCaretLeft } from "react-icons/fa";
 import { FaCaretRight } from "react-icons/fa6";
-import { IoIosRedo } from "react-icons/io";
-import { IoIosUndo } from "react-icons/io";
+
 import {
-        LuHeading1, LuHeading2, LuHeading3, LuListOrdered, LuList,
-        LuFoldVertical, LuType, LuAlignCenter, LuAlignRight, LuAlignLeft 
+        LuHeading1, LuHeading2, LuHeading3, LuListOrdered, LuList, LuTextCursor,
+        LuFoldVertical, LuAlignCenter, LuAlignRight, LuAlignLeft 
       }
 from "react-icons/lu";
+
+import { GrAdd, GrDrag, GrPrevious, GrNext, GrBookmark, GrCatalogOption, GrMore, GrExpand, GrContract } from "react-icons/gr";
 
 //styles
 const IconStyle={borderRadius:'4px', padding:'4px', width:'18px', height:'18px'}
 
+  const ButtonStyle={
+    height:'30px', textAlign:'start', marginLeft:'2.5px', marginRight:'2.5px', borderRadius:'4px',
+    display:'flex', flexDirection:'row', alignItems:'center'
+}
+
 const ipcRenderer = window.require("electron").ipcRenderer;
+
+export function PopUp({close, leftDom, topDom}){
+  const popupRef = useRef(null); //pop up ref
+  const [position, setPosition] = useState({ x: leftDom, y: topDom });
+
+  useEffect(() => {
+    const updatePosition = () => {
+      const { left, top, width, height } = popupRef.current.getBoundingClientRect();
+      const { innerWidth, innerHeight } = window;
+      let newX = position.x;
+      let newY = position.y;
+
+      ipcRenderer.send('message', `Left : ${left}, Top ${top}, Width : ${width}, Height : ${height}, InnerWidth : ${innerWidth}, InnerHeight : ${innerHeight}`)
+
+      // Check if popup overflows on the bottom
+      if (top + height > innerHeight) {
+        newY = innerHeight - (innerHeight - topDom) - height - 50;
+      }
+
+      // Check if popup overflows on the top
+      if (top < 0) {
+        newY = 10;
+      }
+
+      setPosition({ x: newX, y: newY });
+    };
+
+    updatePosition();
+    window.addEventListener('resize', updatePosition);
+
+    return () => {
+      window.removeEventListener('resize', updatePosition);
+    };
+    }, [position]);
+
+  return(
+    <div className='backgroundPopUp'>
+      <div className='backgroundPopUpClose' onClick={close}></div>
+       <div className='popUp' ref={popupRef}
+       style={{left: position.x, top: position.y}}>
+          <button className='popUpBtn'>
+            <LuTextCursor className='popUpIcon' strokeWidth={1}/>
+            <p className='popUpP'><strong>Text</strong><br></br>Basic text</p>
+          </button>
+          <button className='popUpBtn'>
+            <LuHeading1 className='popUpIcon' strokeWidth={1}/>
+            <p className='popUpP'><strong>Header 1</strong><br></br>Titles</p>
+          </button>
+          <button className='popUpBtn'>
+            <LuHeading2 className='popUpIcon' strokeWidth={1}/>
+            <p className='popUpP'><strong>Header 2</strong><br></br>Subtitles</p>
+          </button>
+          <button className='popUpBtn'>
+            <LuHeading3 className='popUpIcon' strokeWidth={1}/>
+            <p className='popUpP'><strong>Header 3</strong><br></br>Larger text</p>
+          </button>
+          <button className='popUpBtn'>
+            <LuListOrdered className='popUpIcon' strokeWidth={1}/>
+            <p className='popUpP'><strong>Number List</strong><br></br>Integers</p>
+          </button>
+          <button className='popUpBtn'>
+            <LuList className='popUpIcon' strokeWidth={1}/>
+            <p className='popUpP'><strong>Ordered List</strong><br></br>Dots</p>
+          </button>
+          <button className='popUpBtn'>
+            <LuFoldVertical className='popUpIcon' strokeWidth={1}/>
+            <p className='popUpP'><strong>Divider</strong><br></br>Border line</p>
+          </button>
+      </div>
+    </div>   
+  )
+}
 
 export default function Page({openPage}) {
 
@@ -37,8 +115,10 @@ export default function Page({openPage}) {
   const [WidthList, setWidthList] = useState('65px')
   const handleWidthList=()=>{if(WidthList==='65px'){setWidthList('170px') }else{setWidthList('65px')}}
 
-  const [hoveringNode, setHoveringNode] = useState({active:false, left:null, top:null})
-  const [characterHover, setCharacterHover] = useState(true)
+  const [hoveringNode, setHoveringNode] = useState({active:false, left:null, top:null}) //hovering button on editor
+  const [characterHover, setCharacterHover] = useState(true) //character/words div
+
+  const [showPopUp, setShowPopUp] = useState(false) //character/words div
 
   const editor = useEditor({
     onUpdate({ editor }){
@@ -69,7 +149,7 @@ export default function Page({openPage}) {
 
   const updateSelection=()=>{
     try {
-      const { $from} = editor.state.selection
+      const {$from} = editor.state.selection
       if($from.parent.type.name === 'paragraph' || 'heading'){
       if ($from.parent.isBlock ) {
         const node = editor.view.nodeDOM($from.before($from.depth))
@@ -96,7 +176,15 @@ export default function Page({openPage}) {
     const { selection } = editor.state;
     const { $from, $to } = selection;
     editor.commands.setTextSelection({ from: $from.start(), to: $to.end()})
-    
+  }
+
+  const addNewContent=()=>{
+    const { selection } = editor.state;
+    const { $to } = selection;
+    editor.commands.focus($to.end())
+    editor.chain().insertContentAt($to.end(), {type: "paragraph"}).focus($to.end()).run()
+    updateSelection()
+    setShowPopUp(true)
   }
 
   if (!editor) {
@@ -105,27 +193,38 @@ export default function Page({openPage}) {
 
   return (
     <div className='Page'>
+      {showPopUp ?
+      <PopUp close={()=>{setShowPopUp(false)}} leftDom={hoveringNode.left} topDom={hoveringNode.top}/>
+      : null}
         <div className='PageInterface'>
           <div className='PageHeader'>
             <div className='PageHeader-left'>
               <div></div>
-              <button className='headerBtn' style={{width:'auto'}}
+              <button className='headerBtn'
               onClick={openPage}>
-                <FaFolderOpen/>
+                <GrCatalogOption size={16}/>
               </button>
-              <button className='headerBtn' style={{width:'auto'}}>
+              <button className='headerBtn'>
                 Save
               </button>
             </div>
             <div className='PageHeader-right'>
-              <button className='headerBtn' style={{width:'27px'}}
+              <button className='headerBtn'
+                >
+                <GrBookmark size={16} color='gold'/>
+              </button>
+              <button className='headerBtn'
                 onClick={() => editor.chain().focus().undo().run()}>
-                <IoIosUndo/>
+                <GrPrevious size={16}/>
               </button>
-              <button className='headerBtn' style={{width:'27px'}}
+              <button className='headerBtn'
                 onClick={() => editor.chain().focus().redo().run()}>
-                <IoIosRedo/>
+                <GrNext size={16}/>
               </button>
+                <button className='headerBtn'
+                >
+                <GrMore size={16}/>
+                </button>
               <div></div>
             </div>
           </div>
@@ -174,17 +273,17 @@ export default function Page({openPage}) {
             <button 
                 style={{minWidth:'35px'}}
                 onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}>
-              <LuHeading1 style={IconStyle}/>
+              <LuHeading1 style={IconStyle} strokeWidth={1.5}/>
             </button>
             <button 
                 style={{minWidth:'35px'}}
                 onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}>
-              <LuHeading2 style={IconStyle}/>
+              <LuHeading2 style={IconStyle} strokeWidth={1.5}/>
             </button>
             <button 
                 style={{minWidth:'35px'}}
                 onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}>
-              <LuHeading3 style={IconStyle}/>
+              <LuHeading3 style={IconStyle} strokeWidth={1.5}/>
             </button>
             </>
             :  null}
@@ -202,17 +301,17 @@ export default function Page({openPage}) {
               <button 
                   style={{minWidth:'35px'}}
                   onClick={() => editor.chain().focus().toggleBulletList().run()}>
-                <LuList style={IconStyle}/>
+                <LuList style={IconStyle} strokeWidth={1.5}/>
               </button>
               <button 
                   style={{minWidth:'35px'}}
                   onClick={() => editor.chain().focus().toggleOrderedList().run()}>
-                <LuListOrdered style={IconStyle}/>
+                <LuListOrdered style={IconStyle} strokeWidth={1.5}/>
               </button>
               <button 
                   style={{minWidth:'35px'}}
                   onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-                <LuFoldVertical style={IconStyle}/>
+                <LuFoldVertical style={IconStyle} strokeWidth={1.5}/>
               </button>
               </>
               :  null}
@@ -230,17 +329,17 @@ export default function Page({openPage}) {
               <button 
                   style={{minWidth:'35px'}}
                   onClick={() => editor.chain().focus().setTextAlign('left').run()}>
-                <LuAlignLeft style={IconStyle}/>
+                <LuAlignLeft style={IconStyle} strokeWidth={1.5}/>
               </button>
               <button 
                   style={{minWidth:'35px'}}
                   onClick={() => editor.chain().focus().setTextAlign('center').run()}>
-                <LuAlignCenter style={IconStyle}/>
+                <LuAlignCenter style={IconStyle} strokeWidth={1.5}/>
               </button>
               <button 
                   style={{minWidth:'35px', borderTopRightRadius:'4px', borderBottomRightRadius:'4px'}}
                   onClick={() => editor.chain().focus().setTextAlign('right').run()}>
-                <LuAlignRight style={IconStyle}/>
+                <LuAlignRight style={IconStyle} strokeWidth={1.5}/>
               </button>
               </>
               :  null}
@@ -253,20 +352,24 @@ export default function Page({openPage}) {
         <>
           <button
           style={{position:'absolute', top:hoveringNode.top, left:hoveringNode.left-40, zIndex:100}}
-          className='selectContentBtn' onClick={()=>{selectContent()}}>+</button>
+          className='selectContentBtn' onClick={()=>{selectContent()}}><GrDrag size={14}/></button>
+          <button
+          style={{position:'absolute', top:hoveringNode.top, left:hoveringNode.left-70, zIndex:100}}
+          className='selectContentBtn' onClick={()=>{addNewContent()}}><GrAdd size={14}/></button>
         </>
         : null }
       
-        <EditorContent editor={editor} />
+        <EditorContent editor={editor}/>
+          
         <div className='PageBottom'>
           <div></div>
           <div className="character-count" onMouseEnter={()=>setCharacterHover(false)} onMouseLeave={()=>setCharacterHover(true)}>
               {characterHover ?
               <div>
-              {editor.storage.characterCount.words()} words
+              {editor.storage.characterCount.words()} Words
               </div> :
               <div>
-              {editor.storage.characterCount.characters()} characters
+              {editor.storage.characterCount.characters()} Characters
               </div>
             }
           </div>
